032da7d13f9b33608975e1cb553fdd17d0d30b36
Wenwen Xie <wxie11@hawk.iie.edu>
2018-01-28 14:29:25 -0600

    add email address validation and alert users the form is submitted

--- pp-01/site.js
+++ pp-01/site.js
@@ -1,1 +1,19 @@
-
+$('#info_form').on("submit",
+  function(e) {
+    var email = $('#email').val();
+    var valid_email = /.+@.+/;
+    e.preventDefault();
+    if(!valid_email.test(email))
+    {
+      console.log('invalid email');
+      $('#invalid').remove();
+      $('#form_list').append('<li id="invalid">Please enter a valid email address.</li>');
+      return false;
+    }
+    else
+    {
+      $(this).remove();
+      $('body').append('<h1>Form is submitted!</h1>');
+    }
+  }
+);
